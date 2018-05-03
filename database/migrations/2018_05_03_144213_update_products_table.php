9f577762ce9e1a47492f36a57546f83f8b3535f7
thoanguyen <vanthoa225@gmail.com>
2018-05-03 15:35:38 +0700

    Edit some fields in the database

---
+++ database/migrations/2018_05_03_144213_update_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function ($table) {
+            $table->renameColumn('cate_id','categories_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
